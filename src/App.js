fff5cc6847f9db6ccc3204ff60eb3a56e34a4652
arjitvermaco <arjitverma.go@gmail.com>
2022-09-16 17:51:25 +0530

    todo app v1 done

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       },
       {
         id: 3,
-        task: "Learn J",
+        task: "Learn JS",
         done: false
       },
       {
@@ -28,56 +28,73 @@ function App() {
     ]
   )
 
+  function handleTodoDelete(todo) {
+    if (window.confirm("Are you sure you want to delete?")) {
+      const finalTodo = todos.filter((t) => {
+        return t.id !== todo.id
+      })
+
+      setTodos(finalTodo)
+    } else {
+      window.alert("Not Deleting")
+    }
+
+
+  }
+
+  function handleAddTodo() {
+    if(todoToAdd === ""){
+      window.alert("Please add todo")
+      return;
+    }else{
+      let newTodo = {
+        id:generateRandomId(),
+        task:todoToAdd,
+        done:false
+      }
+      setTodos([...todos,newTodo])
+      setTodoToAdd("")
+    }
+  }
+
+
+
+  function generateRandomId(){
+    return Math.floor(1000 + Math.random()*9000)
+  }
 
 
   return (
     <div className='body-container'>
       <div className='wrapper'>
         <div className='todo-form'>
-          <input type="text" placeholder="Enter your todo" />
-          <button>Add Todo</button>
+          <input value={todoToAdd} onChange={(event)=>{
+            setTodoToAdd(event.target.value)
+          }} type="text" placeholder="Enter your todo" />
+          <button onClick={handleAddTodo}>Add Todo</button>
         </div>
 
         <div className='todo-list'>
           <ul>
-            <li>Get something some
-              <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="delete-icon">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-              </svg>
-              
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="check-icon">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
 
-              </span>
+            {todos.map((todo, index) => {
+              return (
+                <li key={index}>{todo.task}
+                  <span><svg onClick={() => {
+                    handleTodoDelete(todo)
+                  }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="delete-icon">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                  </svg>
 
-            </li>
+                      {todo.done? (<span className='todo-done'>Done</span>):(<span className='todo-not-done'>Not Done</span>)}
+                    
+                  </span>
 
-            <li>Get something some
-              <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="delete-icon">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-              </svg>
-              
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="check-icon">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
+                </li>
+              )
+            })}
 
-              </span>
 
-            </li>
-
-            <li>Get something some
-              <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="delete-icon">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-              </svg>
-              
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="check-icon">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-
-              </span>
-
-            </li>
           </ul>
         </div>
 
