7342a51684e94dd7114044a6591aba43354cd552
arjitvermaco <arjitverma.go@gmail.com>
2022-09-16 09:29:38 +0530

    Minor change

--- src/App.js
+++ src/App.js
@@ -10,41 +10,19 @@ import LoggedOut from "./LoggedOut";
 //Create a component called APP
 function App() {
 
-    let car = {
-        name :"Nexon",
-        brand:"Tata",
-        color:"WHite",
-        year:2018
-    }
-
-    let fullName = "arjit";
-
-    let userLoggedIn = true;
-    let counter = 0;
-
-    function logButtonClick(){
-        counter++;
-        console.log(counter);
-    }    
 
 
 
     return (
    
       <div className="body-container">
-        <h2>My name is {fullName ? (fullName):("Stranger")}</h2>
-        <h4>My car is {car.name}</h4>
-        <p>{car.brand}</p>
-        <p>{car.color}</p>
-        <p>{car.year < 2020 ? ("Old car"):("New Car")}</p>
-        <button onClick={logButtonClick}>Click Me</button>
-        {userLoggedIn? <LoggedIn/>:<LoggedOut/>}
+    
 
-        {/* <Header/>
+        <Header/>
         <DownloadBtn/>
         <Feature1/>
         <Feature2/>
-        <Feature3/> */}
+        <Feature3/>
       </div>
      
     )
